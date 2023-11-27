ee48a3f0faad10b66653f09db60f309b364753dd
satyam4321 <satyam@an-associates.com>
2023-11-28 02:08:04 +0530

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const authRoute=require('./routes/auth.js')
 const userRoute=require('./routes/users.js')
 const postRoute=require('./routes/posts.js')
 const commentRoute=require('./routes/comments.js')
-const BASE_URL = process.env.BASE;
+const BASE_URL = process.env.BASE_URL;
 // const PORT = process.env.PORT || 3000;
 dotenv.config()
 //database
@@ -27,7 +27,12 @@ const connectDB=async()=>{
 //middlewares
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:{BASE_URL},credentials:true}))
+app.use(cors({
+    origin: {BASE_URL}
+}
+))
+app.options('*', cors())
+// app.use(cors({origin:{BASE_URL},credentials:true}))
 // app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
